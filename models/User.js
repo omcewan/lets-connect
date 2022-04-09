e1ed94cca08d5119bf795507822a2bfd59bfe772
omcewan <orlandomcewan231@gmail.com>
2022-04-08 22:16:33 -0400

    create User model and a validate email function

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+const validateEmail = require('../utils/validateEmail');
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: [validateEmail, 'Please enter a valid email'],
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought',
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+});
+
+UserSchema.virtual('friendCount').get(() => {
+  return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
